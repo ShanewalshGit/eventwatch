45619ea5545f119eeab26fec3b5c23e991ae158d
Shane Walsh <swalsh818@gmail.com>
2024-09-09 14:28:13 +0100

    File uploader, date picker, Form submission and actions

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -19,6 +19,7 @@ import {
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
   import { Input } from "../ui/input"
+import { createCategory, getAllCategories } from "@/lib/actions/category.actions"
   
   type DropdownProps = {
     value?: string
@@ -30,9 +31,23 @@ import {
     const [newCategory, setNewCategory] = useState('');
   
     const handleAddCategory = () => {
-      
+      createCategory({
+        categoryName: newCategory.trim()
+      })
+        .then((category) => {
+          setCategories((prevState) => [...prevState, category])
+        })
     }
+
+    useEffect(() => {
+      const getCategories = async () => {
+        const categoryList = await getAllCategories();
   
+        categoryList && setCategories(categoryList as ICategory[])
+      }
+  
+      getCategories();
+    }, [])  
     return (
       <Select onValueChange={onChangeHandler} defaultValue={value}>
         <SelectTrigger className="select-field">
